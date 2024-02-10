21cef7b2381d85c035fc5502afbf0bfd510dcdf1
MohitSingh7979 <mohitsinghatwork@gmail.com>
2024-02-11 03:19:33 +0530

    rename variables and solved some bugs

--- ASSIGNMENTS/assign_3_js/assignment_3.js
+++ ASSIGNMENTS/assign_3_js/assignment_3.js
@@ -1,163 +1,209 @@
 //for better understanding in console printing '=' 20 times
-function quesEndLine () {
-	const smbl = `=`.repeat(20);
-	console.log(`${ smbl }\n\n`);
+
+//helper funciton
+
+function secEndLine() {
+	const sym = "=".repeat(20);
+	console.log(`${ sym }\n\n`);
 }
 
-function promptMultipleNumbers () {
+//utility function
+
+function inpMultNums() {
 	const arr = [];
-	let range = prompt(`Enter range/length of array you wish to have `);
-	for (let index = 0; index < range; index++) {
-		let number = parseInt(prompt(`Please Enter a Number`));
-		arr.push(number);
+	const range = prompt("Enter range/length of array you wish to have");
+	for (let i = 0; i < range; i++) {
+		const num = parseInt(prompt("Please Enter a Number"));
+		!isNaN(num) ? arr.push(num) : 0;
 	}
 	return arr;
 }
 
-// ----------------------------------
-/* console.log(
-	`1. Write a JavaScript function to calculate the sum of two numbers.`,
-);
+//utility function
 
-function calculateSum(num1, num2) {
-	return parseInt(num1) + parseInt(num2);
+function empEnt() {
+	const name = prompt("Enter Name of the Employee");
+
+	let salary = parseInt(prompt("Enter Salary of the Employee"));
+	salary = isNaN(salary) ? 0 : salary;
+
+	const dept = prompt("Enter Department of the Employee");
+
+	return { name, salary, dept };
 }
-let number1 = prompt(`Enter first number`),
-	number2 = prompt(`Enter Second Number`);
 
-const sumResult = calculateSum(number1, number2);
+function ques(params) {
+	console.log("Question:", params);
+}
 
-console.log(`Sum of numbers ${number1} and ${number1} is ${sumResult}`);
-quesEndLine(); */
-// ----------------------------------
+function res(params) {
+	console.log(params);
+}
 
-// ----------------------------------
-/* console.log(
-	`2. Write a JavaScript program to find the maximum number in an array.`,
-);
+// question-1
 
-const userInputs1 = promptMultipleNumbers();
-function findMaxNumber(arr) {
-	let max = arr[0];
-	for (const item of arr) {
-		if (max < item) {
-			max = item;
+(function () {
+	ques("1. Write a JavaScript function to calculate the sum of two numbers.");
+
+	function calcSum(num1, num2) {
+		if (!isNaN(num1) && !isNaN(num2)) {
+			return num1 + num2;
 		}
 	}
-	return max;
-}
 
-const maxResult = findMaxNumber(userInputs1);
+	const num1 = 0 | parseInt(prompt("Enter first number"));
+	const num2 = 0 | parseInt(prompt("Enter Second Number"));
 
-console.log(` maximum number is ${maxResult} from ${userInputs1}`);
-quesEndLine(); */
-// ----------------------------------
+	const sumRes = calcSum(num1, num2);
 
-// ----------------------------------
-/* console.log(
-	`3. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.`,
-);
-let userInputs2 = promptMultipleNumbers();
+	res(`Sum of numbers ${ num1 } and ${ num2 } is ${ sumRes }`);
+	secEndLine();
+})();
 
-function getEvenNumbers(arr) {
-	arr.forEach((element) => {
-		if (element % 2 != 0) {
-			arr.pop(element);
+//question-2
+
+(function () {
+	ques("2. Write a JavaScript program to find the maximum number in an array.");
+
+	const usrInps = inpMultNums();
+
+	function findMaxNum(nums) {
+		let max = nums[ 0 ];
+		for (let i = 1; i < usrInps.length; i += 1) {
+			if (nums[ i ] > max) {
+				max = nums[ i ];
+			}
 		}
+		return max;
+	}
+
+	const maxNum = findMaxNum(usrInps);
+
+	res(` maximum number is ${ maxNum } from ${ usrInps }`);
+	secEndLine();
+})();
+
+//question-3
+
+(function () {
+	ques(
+		"3. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.",
+	);
+	let usrInps = inpMultNums();
+
+	function getEvnNums(nums) {
+		const newArr = nums.filter((num) => num % 2 === 0);
+		return newArr;
+	}
+
+	const evnNums = getEvnNums(usrInps);
+
+	res(`Even numbers are ${ evnNums } from ${ usrInps }`);
+	secEndLine();
+})();
+
+//question-4
+
+(function () {
+	ques(
+		"4. Write a function that takes an array of integers as input and returns a new array with only the unique elements.",
+	);
+
+	const usrInps = inpMultNums();
+
+	function getUniqNums(nums) {
+		const uniqNums = [];
+
+		nums.forEach((currItem) => {
+			if (!uniqNums.includes(currItem)) {
+				uniqNums.push(currItem);
+			}
+		});
+
+		return uniqNums;
+	}
+
+	const uniqNums = getUniqNums(usrInps);
+
+	console.log(`Unique numbers are ${ uniqNums } from numbers ${ usrInps }}`);
+
+	secEndLine();
+})();
+
+//question-5
+
+(function () {
+	ques("5. How do you add an element to the end of an array?");
+	const usrInps = inpMultNums();
+
+	console.log(`Your Array : ${ usrInps }`);
+
+	const pushElems = parseInt(
+		prompt("Enter Element to add at the end of the array"),
+	);
+
+	usrInps.push(pushElems);
+	console.log(`Updated Array : ${ usrInps }`);
+
+	secEndLine();
+})();
+
+//question-6
+
+(function () {
+	ques("6. How do you remove an element from an array at a specific index?");
+
+	const usrInps = inpMultNums();
+	console.log(`Your Array : ${ usrInps }`);
+
+	const rmvIdx = parseInt(prompt("Enter index from which element should be removed"));
+
+	function rmvElem(arr, idx) {
+		arr.splice(idx, 1);
+	}
+
+	rmvElem(usrInps, rmvIdx);
+
+	console.log(`Updated Array : ${ usrInps }`);
+
+	secEndLine();
+})();
+
+//question-7
+
+(function () {
+	ques(
+		"7. Write a program to create 5 employees data in object and print all data using map function.",
+	);
+
+	const emplsDat = [];
+
+	for (let i = 0; i < 1; i++) {
+		emplsDat.push(empEnt());
+	}
+
+	const emplsInfo = emplsDat.map((data) =>
+		console.log(
+			`Name: ${ data.name }		Salary: ${ data.salary }		Department: ${ data.dept }`,
+		),
+	);
+
+	secEndLine();
+})();
+
+//question-8
+
+(function () {
+	ques("8. W.A.P. to print all employees' data whose salary is greater than 20000 and dept is sales.");
+
+	const emplsDat = [];
+
+	const filtEmplsInfo = emplsDat.filter((data) => {
+		return data.salary > 0;
 	});
-	return arr;
-}
 
-console.log(
-	`Even numbers are ${getEvenNumbers(
-		userInputs2,
-	)} from array of ${userInputs2}`,
-);
-quesEndLine(); */
-// ----------------------------------
+	const emplsInfo = filtEmplsInfo;
+	console.log(emplsInfo);
+	secEndLine();
 
-// ----------------------------------
-/* console.log(
-	`4. Write a function that takes an array of integers as input and returns a new array with only the unique elements.`,
-);
-
-const userInputs3 = promptMultipleNumbers();
-
-function getUniqueNumbers (arr) {
-	const uniqueNumbers = [];
-
-	arr.forEach((currentItem) => {
-		if (!uniqueNumbers.includes(currentItem)) {
-			uniqueNumbers.push(currentItem);
-		}
-	});
-
-	return uniqueNumbers;
-}
-
-console.log(
-	`Unique numbers are ${ getUniqueNumbers(
-		userInputs3,
-	) } from numbers ${ userInputs3 }}`,
-);
-
-quesEndLine(); */
-// ----------------------------------
-
-// ----------------------------------
-/* console.log(`5. How do you add an element to the end of an array?`);
-const userInputs4 = promptMultipleNumbers();
-
-console.log(`Your Array : ${userInputs4}`);
-
-const pushElements = parseInt(
-	prompt("Enter Element to add at the end of the array"),
-);
-
-userInputs4.push(pushElements);
-console.log(`Updated Array : ${userInputs4}`);
-
-quesEndLine(); */
-// ----------------------------------
-
-// ----------------------------------
-// console.log(
-// 	`6. How do you remove an element from an array at a specific index?`,
-// );
-
-// const userInputs5 = promptMultipleNumbers();
-// console.log(`Your Array : ${ userInputs5 }`);
-
-// const removeElement = parseInt(prompt(`Enter index from which element should be removed`));
-
-// userInputs5.pop();
-
-// console.log(`Updated Array : ${ userInputs5 }`);
-
-// quesEndLine();
-// ----------------------------------
-
-// ----------------------------------
-console.log(
-	`7. Write a program to create 5 employees data in object and print all data using map function.`,
-);
-
-const employeesData = [];
-
-for (let index = 0; index < 2; index++) {
-	let name = prompt("Enter Name of the Employee");
-	let salary = prompt("Enter Salary of the Employee");
-	let dept = prompt("Enter Department of the Employee");
-	employeesData.push({ name, salary, dept });
-}
-
-const employeesInfo = employeesData.map((data) => console.log(`Name: ${ data.name }		Salary: ${ data.salary }		Department: ${ data.dept }`));
-quesEndLine();
-// ----------------------------------
-
-// ----------------------------------
-console.log(
-	`8. Wap to print all employees' data whose salary is greater than 20000 and dept is sales.`,
-);
-quesEndLine();
-// ----------------------------------
+})();
---
+++ ASSIGNMENTS/assign_3_js/readme.md
@@ -0,0 +1,32 @@
+# suggestions or Improvement way
+
+1. use 1 only if want to pass the variable
+2. use more const instead of let
+3. variable = max 3 char
+4. \*alert instead of prompt
+5. parseInt when taking input from user
+6. long statement should be isolated
+7. func meaningfull name
+8. also func and params should be matching
+9. if condition checker comes first
+10. maxNumResult : 50 line
+11. question 3
+12. use filter instead of arr.pop
+13. don't operate on arr while iterating (focus)
+14. don't pass direct function in tamplate string
+15. use only variable in tamplate string
+16. skip () in 1 params in arrow function `()=>{}`
+17. reduce, set, object for unique
+18. 117
+19. use function for employeeData
+20. study map more in depth
+21. 7 not completed.
+22. escape char read more.
+23. use of comment for blocks or function
+24. separate helper function with good comment at top or separate file.
+25. build structure first: helper, utilities functions
+
+use more of **IIFE** for safety : syntax
+`const {variableName} = (function () { return {variableName} })()`
+
+27. lexical scoping
