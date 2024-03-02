07f40d49592d6c47144a30fad61d3fc0966bbf22
Mohit Singh <mohitsinghatwork@gmail.com>
2024-03-02 11:01:07 +0530

    added refined assign calc

--- ASSIGNMENTS/assign_5_calc/script.js
+++ ASSIGNMENTS/assign_5_calc/script.js
@@ -1,22 +1,27 @@
-const num1Elem = document.getElementById("num1");
-const num2Elem = document.getElementById("num2");
+const num1Elem = document.getElementById("num-1");
+const num2Elem = document.getElementById("num-2");
 const resElem = document.getElementById("res");
 
-const sumElem = document.getElementById("btn_sum");
-const minElem = document.getElementById("btn_min");
-const divElem = document.getElementById("btn_div");
-const multElem = document.getElementById("btn_mult");
+const btnWrapper = document.getElementById("btn-wrapper");
 
-const sum = () => {
-  resElem.value = Number(num1Elem.value) + Number(num2Elem.value);
+btnWrapper.addEventListener( "click", function ( eve ) {
+	eve.preventDefault();
 
-};
-const min = () => {
-	resElem.value = num1Elem.value - num2Elem.value;
-};
-const div = () => {
-	resElem.value = num1Elem.value / num2Elem.value;
-};
-const mult = () => {
-	resElem.value = num1Elem.value * num2Elem.value;
-};
+	const num1Val = Number(num1Elem.value);
+	const num2Val = Number( num2Elem.value )
+
+	switch (eve.target.getAttribute("id")) {
+		case "btn-sum":
+				resElem.value =  num1Val + num2Val ;
+			break;
+		case "btn-min":
+				resElem.value =  num1Val - num2Val ;
+			break;
+		case "btn-div":
+				resElem.value =  num1Val / num2Val ;
+			break;
+		case "btn-mult":
+				resElem.value =  num1Val * num2Val ;
+			break;
+	}
+} )
