566f6d4a1745cb53e384cd951c359ecbb1f956b5
MohitSingh7979 <mohitsinghatwork@gmail.com>
2024-03-06 18:10:59 +0530

    unwanted commit

--- ASSIGNMENTS/assign_4_clock/script.js
+++ ASSIGNMENTS/assign_4_clock/script.js
@@ -3,7 +3,7 @@ const dt = new Date();
 const dtElem = document.getElementById("date");
 const tmElem = document.getElementById("time");
 
-const wkElem = dt.getDay();
+const wkElem = dt.toLocaleDateString();
 const mnthElem = dt.getMonth();
 const dyElem = dt.getDate();
 const fyElem = dt.getFullYear();
@@ -11,9 +11,6 @@ const fyElem = dt.getFullYear();
 const hrElem = dt.getHours();
 const minElem = dt.getMinutes();
 const secElem = dt.getSeconds();
-
-const arrDay = ["Monday","Tuesday", "Webnesday",]
-const arrMnth = ["January", "February", "March", "April","June", "July", "August", "September","October", "November", "December"]
 
 dtElem.textContent = wkElem + " " + mnthElem + " " + dyElem + " " + fyElem;
 tmElem.textContent = hrElem + " " + minElem + " " + secElem;
